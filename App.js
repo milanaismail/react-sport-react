470f3a9552285da78eded26e14d44f97660d5531
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-01-03 21:31:31 +0100

    package

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
